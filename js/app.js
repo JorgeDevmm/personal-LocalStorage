374c1b9962bacf70575c33998eefe6dfb7ad41aa
JorgeDevmm <jorge29luis10@gmail.com>
2023-11-29 12:16:00 -0500

    Agregando funciones eliminarMensajes,LimpiarHtml y boton borran

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ eventListeners();
 
 function eventListeners() {
   formulario.addEventListener('submit', agregarTweet);
+  listaTweets.addEventListener('click', eliminarMensaje);
 }
 
 //TODO Funciones
@@ -21,15 +22,28 @@ function agregarTweet(e) {
 
   // Validación de uqe no se puede entrar un dato vacio
   if (tweet === '') {
-    mostrarError('Un mensaje no puede ir vacío');
+    mostrarError('Un mensaje no puede ingresar vacío');
 
     return; //evita que se ejecuten más lineas de código
   }
-  tweets.push(tweet);
 
-  console.log(tweets);
+  // para evitar duplicidad agregamos como objeto commo Date.now como id
+  const tweetObj = {
+    id: Date.now(),
+    tweet,
+  };
+
+  // Añadiendo al arreglo de tweets
+  tweets = [...tweets, tweetObj];
+
+  // Crear Html
+  crearHtml();
+
+  // Reiniciar el formulario
+  formulario.reset();
 }
 
+// mostrar alerta de error
 function mostrarError(error) {
   limpiarAlertaFinal(contenido);
 
@@ -47,10 +61,61 @@ function mostrarError(error) {
   }, 3000);
 }
 
+// limpiar alertas previas
 function limpiarAlertaFinal(referencia) {
   // Comprueba si ya existe una alerta, en la referencia y la descendencia
   const alerta = referencia.querySelector('.error');
   if (alerta) {
     alerta.remove();
+  }
+}
+
+function crearHtml() {
+  // Limpiar html
+  lipiarHtml();
+
+  // validar elementos dentro del arreglo
+  if (tweets.length > 0) {
+    // recorrer el arreglo de tweets, y crear elemento html y mostrarlo en la lista
+    tweets.forEach((objMensaje) => {
+      const mensaje = document.createElement('P');
+      const borrar = document.createElement('span');
+      mensaje.textContent = objMensaje.tweet;
+      mensaje.classList.add('background-gris', 'p-05', 'font-w');
+
+      // establezco clases y atributos para boton borrar
+      borrar.textContent = 'X';
+      borrar.classList.add('borrar-tweet');
+      borrar.setAttribute('data-id', objMensaje.id);
+
+      listaTweets.appendChild(mensaje);
+      mensaje.appendChild(borrar);
+    });
+  }
+
+  console.log(tweets);
+}
+
+// Eliminar los cursos de la listaTweets
+function lipiarHtml() {
+  // si contenedor tiene al menos un elemento
+  while (listaTweets.firstChild) {
+    // eliminar un hijo por el primero
+    listaTweets.removeChild(listaTweets.firstChild);
+  }
+}
+
+// ELiminar Mensaje
+function eliminarMensaje(e) {
+  // verificamos si se encuentra la clase donde presionamos
+  if (e.target.classList.contains('borrar-tweet')) {
+    // guardamos el id de mensaje a eliminar
+    const mensajId = e.target.getAttribute('data-id');
+
+    // guardamos en un nuevo array el los elementos diferente al id eliminado
+    tweets = tweets.filter((mensaje) => mensaje.id != mensajId);
+
+    // volvemos a generar html
+    crearHtml();
   }
 }
